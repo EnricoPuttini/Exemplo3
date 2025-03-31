3279184fe28dcd154b4035d51fd32b7cca1460d4
Enrico <rico.set06@gmail.com>
2025-03-31 09:29:22 -0300

    conhecendo alguns metodos

---
+++ src/js/script.js
@@ -0,0 +1,22 @@
+//DECLARAÇOES E VARIAVEIS
+
+var nome = "fiap";
+console.log(nome);
+
+let idade = 18;
+console.log(idade);
+
+//METODOS
+
+let frase = "O mundo da tecnologia";
+console.log(frase.lenght);
+
+//indexO - retorna um trecho no texto
+
+let texto= "A programação indomável e sustentavel";
+console.log(texto.indexOf("ão"))
+
+//slice - retorna um trecho do texto com inicio e final
+
+let info ="Processamento de ponta";
+console.log(info.slice(16,18))
