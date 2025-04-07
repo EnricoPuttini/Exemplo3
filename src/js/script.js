d03bcaf7f8ab85565765704e44500d8bdc0dd842
Laboratório FIAP <labsfiap@fiap.com.br>
2025-04-07 09:17:41 -0300

    Criando condicionais com if, else e if/else e swit ch case

--- src/js/script.js
+++ src/js/script.js
@@ -1,45 +1,89 @@
-//DECLARAÇOES E VARIAVEIS
+// //DECLARAÇOES E VARIAVEIS
 
-var nome = "fiap";
-console.log(nome);
+// var nome = "fiap";
+// console.log(nome);
 
-let idade = 18;
-console.log(idade);
+// let idade = 18;
+// console.log(idade);
 
-//METODOS
+// //METODOS
 
-let frase = "O mundo da tecnologia";
-console.log(frase.lenght);
+// let frase = "O mundo da tecnologia";
+// console.log(frase.lenght);
 
-//indexO - retorna um trecho no texto
+// //indexO - retorna um trecho no texto
 
-let texto= "A programação indomável e sustentavel";
-console.log(texto.indexOf("ão"))
+// let texto= "A programação indomável e sustentavel";
+// console.log(texto.indexOf("ão"))
 
-//slice - retorna um trecho do texto com inicio e final
+// //slice - retorna um trecho do texto com inicio e final
 
-let info ="Processamento de ponta";
-console.log(info.slice(16,18))
+// let info ="Processamento de ponta";
+// console.log(info.slice(16,18))
 
-//OPERADORES ARITIMÉTICOS
+// //OPERADORES ARITIMÉTICOS
 
-const num1=10;
-const num2=20;
+// const num1=10;
+// const num2=20;
 
-console.log(num1 + num2);
-console.log(num1 - num2);
-console.log(num1 * num2);
-console.log(num1 / num2);
+// console.log(num1 + num2);
+// console.log(num1 - num2);
+// console.log(num1 * num2);
+// console.log(num1 / num2);
 
-//OPERADORES LÓGICOS
+// //OPERADORES LÓGICOS
 
-const num3=10;
-const num4=20;
+// const num3=10;
+// const num4=20;
 
-console.log(num3 < num4)
-console.log(num3 > num4 && num4 < num3)
-console.log(num3 < num4 || num4 < num3)
+// console.log(num3 < num4)
+// console.log(num3 > num4 && num4 < num3)
+// console.log(num3 < num4 || num4 < num3)
 
-//OPERADORES DE COMPARAÇÃO
+// //OPERADORES DE COMPARAÇÃO
 
-console.log(num2 == num4)
+// console.log(num2 == num4)
+// console.log(num2 === num4)
+
+// //if/else
+
+// let usuario = "teste"
+
+// if(usuario == "teste"){
+//     console.log("Usuario correto")
+// }else{
+//     console.log("usuario Errado")
+// }
+
+// if encadeado / aninhado
+
+// let idade = 15
+
+// if(idade <= 15){
+//     console.log("Menor de idade")
+// }else if(idade > 15 && idade < 18){
+//     console.log("não é maior mas pode entrar")
+// }else{
+//     console.log("Pode entrar é maior")
+// }
+
+// // switch
+
+// let time = "Palmeiras"
+
+// switch(time){
+//     case "Palmeiras":
+//         console.log("Melhor time")
+//         break;
+//     case "Sao Paulo":
+//         console.log("Pior time")
+//         break;
+//     case "Corinthians":
+//         console.log("Quarta forca de sp")
+//         break;
+//     default:
+//         console.log("nenhuma das opcções")
+// }
+
+//ternario
+
